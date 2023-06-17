16b9d47a13f9bca294b67c1de4d4ba1c661dc245
quinnrs <95441331+quinnrs@users.noreply.github.com>
2023-06-17 09:00:51 -0400

    Add files via upload

---
+++ Ex_10-9-11.py
@@ -0,0 +1,9 @@
+#
+# new file named fin to read input
+
+fin = open("Chapter_9/words.txt")
+# # print(fin.readline()) # will stop at first new line
+# read the next line # returns expected value
+# print(fin.readline())
+
+print("\nEx_10-9")
